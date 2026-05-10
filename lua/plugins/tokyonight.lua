60628d6b0d5009fd4c8775c510153076d135e505
Michael Clark <mc2.dev@pm.me>
2026-05-09 19:01:58 -0500

    brighten line numbers

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -2,6 +2,21 @@ return {
   {
     "folke/tokyonight.nvim",
     lazy = true,
-    opts = { style = "night", dim_inactive = true },
+    opts = {
+      style = "night",
+      dim_inactive = true,
+      on_highlights = function(hl, c)
+        hl.LineNr = {
+          fg = c.comment,
+        }
+
+        hl.LineNrAbove = {
+          fg = c.comment,
+        }
+        hl.LineNrBelow = {
+          fg = c.comment,
+        }
+      end,
+    },
   },
 }
